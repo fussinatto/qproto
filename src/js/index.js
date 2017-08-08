fb266d11700cd53019b255f45ce128bd057f416f
Dario <fussinatto@gmail.com>
2017-08-08 21:35:44 +0200

    init commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+const Day = require('./modules/Day')
+
+let newDay = new Day()
+newDay.init('Something from New Day')
---
+++ src/js/modules/Day.js
@@ -0,0 +1,7 @@
+class Day {
+  init (arg) {
+    console.log('Logged: ' + arg)
+  }
+}
+
+export default Day
